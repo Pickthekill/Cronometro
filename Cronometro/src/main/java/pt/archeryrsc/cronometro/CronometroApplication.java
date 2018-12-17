915e340b5dac64c858e06e8fef86dfd9e9bdd967
Pickthekill <didi99@live.com.pt>
2018-12-17 20:48:14 +0000

    projeto

---
+++ Cronometro/src/main/java/pt/archeryrsc/cronometro/CronometroApplication.java
@@ -0,0 +1,14 @@
+package pt.archeryrsc.cronometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronometroApplication.class, args);
+	}
+
+}
+
